b527a79715a4d8013b6ea424ffbf23b5df50b1a0
Janis <j.chelli@gmail.com>
2017-11-25 10:55:16 -0200

    Commit inicial

---
+++ jquery.js
@@ -0,0 +1,12 @@
+/* JQuery */
+
+//funcao clicar no botao
+function mudar(){
+    var texto = $(".titulo")
+    alert(texto.html());
+}
+// Carregar sem clicar
+$(document).ready(function(){
+var texto = $(".titulo")
+alert(texto.html());
+});
