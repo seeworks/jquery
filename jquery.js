5b80e5a1e88e8120475260b03b0beb2a8cbac20a
Janis <j.chelli@gmail.com>
2017-11-25 12:25:14 -0200

    Nova alteracao

--- jquery.js
+++ jquery.js
@@ -1,12 +1,55 @@
 /* JQuery */
 
 //funcao clicar no botao
-function mudar(){
+/*function mudar(){
     var texto = $(".titulo")
     alert(texto.html());
 }
 // Carregar sem clicar
 $(document).ready(function(){
-var texto = $(".titulo")
-alert(texto.html());
+var texto = $("div")
+console.log(texto);
+$("#mudar").click(mudar);
+$("#mudar").click(function(){
+    $("div").css("background-color","yellow"); 
 });
+
+});
+// colocar cor no elemento h1
+function mudar(){
+ $("h1").css("background-color","orange");
+ $("div").css("background-color","red");
+}
+
+//pode clicar em qualquer area
+$(document).ready(function(){
+    $("div, h1").click(function(){
+        $(this).css("background-color","orange");
+    });
+});
+*/
+// adicionar cidades na <li>
+$(document).ready(function () {
+
+    $("div, h1").click(function () {
+        $(this).css("background-color", "silver");
+    });
+
+    $("#mudar").click(function () {
+        var nome = $("#nome").val();
+        var novo = $("<li>" + nome + "</li>").click(function () {
+            $(this).fadeIn("slow", function(){
+                $(this).remove();
+            });
+            
+        });
+
+        $( "<span> X </span>").click(function(){
+            // fica pra proxima aula
+        });
+
+        $("ul").append(novo);
+    });
+
+});
+
